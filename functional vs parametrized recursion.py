db8bfca4ff36a059738b292e1ba0f025e2a2da45
Sahasrika_Gowrishetty <sahasrikagowrishetty@gmail.com>
2026-07-10 23:15:26 +0530

    Add files via upload

---
+++ functional vs parametrized recursion.py
@@ -0,0 +1,15 @@
+print("------------PARAMETRIZED FUNCTION IN RECURSION------------")
+def function(sum,i,n):
+    if i>n:
+        return
+    print(sum)
+    function(sum+i,i+1,n)
+function(0,1,10)
+print("--------------FUNCTIONAL RECURSION--------------")
+#check for base and flow while using recursion
+def functions(sum,i,s):
+    if i>n:
+        return 1
+    print(sum)
+    function(sum+i,i+1,n)
+function(0,1,10)
