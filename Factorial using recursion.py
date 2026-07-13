aaa1269c06a6c3f0d763350b6b650cabc2efb026
Sahasrika_Gowrishetty <sahasrikagowrishetty@gmail.com>
2026-07-13 11:52:39 +0530

    Implement factorial using recursion and for loops
    Added a factorial calculation using for loops.

--- Factorial using recursion.py
+++ Factorial using recursion.py
@@ -5,3 +5,11 @@ def function(fact,n,i):
         return 
     function(fact*n,n-1,i+1)
 function(1,5,0)    
+print("-----------FACTORIAL USING FOR LOOPS------")
+n=10
+factorial = 1
+for i in range(1,n+1):
+    factorial = factorial * i
+print(factorial)
+    
+    
